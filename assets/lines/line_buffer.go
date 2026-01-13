65507a6bd6e05c04597e7627e88ee17b12cdf1c9
dlannan <dlannan_temp@gagagames.com>
2026-01-14 01:15:48 +1030

    Working method using plain vertex array only. VERY fast.

---
+++ assets/lines/line_buffer.go
@@ -0,0 +1,9 @@
+embedded_components {
+  id: "lines_mesh"
+  type: "mesh"
+  data: "material: \"/assets/lines/lines_buffer.material\"\n"
+  "vertices: \"/assets/lines/lines_temp.buffer\"\n"
+  "position_stream: \"position\"\n"
+  "normal_stream: \"position\"\n"
+  ""
+}
---
+++ assets/lines/lines_buffer.fp
@@ -0,0 +1,9 @@
+#version 140
+
+in vec4 out_color;
+out vec4 out_fragcolor;
+
+void main()
+{
+	out_fragcolor = out_color;
+}
